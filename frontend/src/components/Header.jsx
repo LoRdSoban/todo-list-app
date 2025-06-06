90478bbad0ed561c8170058a0e339ebd9bc090e6
M. Soban Amir <62153801+LoRdSoban@users.noreply.github.com>
2025-06-06 18:53:40 +0500

    add profile pic

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,11 @@ const Header = () => {
     <div className="text-center mb-8">
       <div className="w-20 h-20 mx-auto mb-4 rounded-full overflow-hidden border-4 border-white shadow-lg">
         {/*TODO: Add profile picture here*/}
+        <img 
+          src="https://st.depositphotos.com/1779253/5140/v/450/depositphotos_51405259-stock-illustration-male-avatar-profile-picture-use.jpg" 
+          alt="Profile" 
+          className="w-full h-full object-cover"
+        />
       </div>
     </div>
   );
